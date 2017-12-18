4d69eae0f4ea1b654e9dc0c0202c66a12b116313
Martin Bugayong <martinbugayong@Martins-MacBook-Pro.local>
2017-12-18 11:45:17 -0800

    updated

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
 # connectFour
+
+# MVP
+- Game board 
+- Turns
+- Win conditions 
+
+# _Bonus (if time allows)_ 
+- Name setter
+- Color picker  
+- Keyboard and mouse compatability 
+
+# MVP 
+### 1. Game Board 
+- Colums of 7 and rows of 6 (Array)
+- The tiles will be able to become the color the player is assigned
+- Ensuring a (stacking) feature so players can not change the color of an opponents placement
+- Ensuring placement accuracy
+- If column is full becomes disabled
+
+### 2. Turns 
+- Player 1 will always go first 
+- Will always alternate until win conditions have been met 
+- Will display winning player prompt 
+
+### 3. Win conditions 
+- 4 in a row, column, or diagonally
+- In an event of a tie game players will be able to reset the board 
+
+# _Bonus (if time allows)_
+
+### Turns 
+- Timer of 3 seconds per turn to ensure fast games _(quick maths)_
+- 1st turn randomizer 
+    - Coin flip to determine who goes fist. 
+        - Coin will be color coded acording to players selection
+- score keeper 
+- leaderboard
+### Name setter 
+- Enable a prompt or alert that has inputs and sets player's name accordingly
+
+### Color picker 
+- While the prompt of setting players name is up they will be able to choose the color of their liking. 
+- RGB 
+
+### Keyboard and mouse compatability 
+- Arrow keys and mouse to make multiplayer easier 
+- Highlighting colums to ensure player using arrow key's has accurate placement
+- Pressing the down arrow will set the tile. 
+ 
+
+some code that generates a board in javascript
+
+Making div columns and rows in JS. avoiding hardcoding 7/6 grid 
+var funLoop = function() {
+    for (var i = 0; i < 7; i++) {
+        var html = "<div class='column'>"
+        for (var j= 0; j < 6; j++) {
+            html += "<div class='row'>" + j + "</div>"
+        }
+        html+= i+ "<div>"
+        return(html)
+    }
+}
+
+then take the string and add this to a div with a class or id as the innerHTML
+document.querySelector('#board).innerHTML = funLoop()
+
+then add .css to format rows and columns, for things with class of row or column from display being either block or inline
+
+creating the data for the board as an array of arrays
+// var board = [
+[0,1,2,3], [4,5,6,7], [8,9,10,11] ]
--- js/main.js
+++ js/main.js
@@ -3,22 +3,19 @@
 // === ---> COMPARES but STRICTER, must be same type. (1 === true) is false
 
 /*----- constants -----*/
+// create your constant variables. These will never change. 
 var player1 = 'r';
-var player2 = 'y';
-// /*----- app's state (variables) -----*/
+var player2 = 'b';
+// /*----- app's state (variables) -----*/ 
 var whosTurn;
 var board;
 var winner;
 
 /*----- cached element references -----*/
 
-/*----- event listeners -----*/
-//Modify dom, select the board and add click event listener.
-// var btn = document.querySelector('board')
-//     btn.addEventListiner('click', click);
-
 
 /*----- functions -----*/
+// Create an initialize function with a board array of columns and rows. 
 function init() {
     board = [
         ['_', '_', '_', '_', '_', '_'],
@@ -33,7 +30,7 @@ function init() {
     winner = false;
 }
 
-//Define a variable expression and add an event listiner.
+/*----- event listeners -----*/
 document.getElementById('col0').addEventListener('click', handleClick)
 document.getElementById('col1').addEventListener('click', handleClick)
 document.getElementById('col2').addEventListener('click', handleClick)
@@ -43,12 +40,12 @@ document.getElementById('col5').addEventListener('click', handleClick)
 document.getElementById('col6').addEventListener('click', handleClick)
 
 function handleClick(e) {
-    var currentColumnId = e.currentTarget.id; // 'col2': string
-    var currentColumnIndex = currentColumnId[3]; // '2': string
-    var currentColumn = board[currentColumnIndex]; // ['y', 'y', 'r', '_', '_', '_']: array
+    var currentColumnId = e.currentTarget.id; // column id is set to current target clicked and will grab  the ID from the html
+    var currentColumnIndex = currentColumnId[3]; // which ever column is selected will return 3rd indexed character in string 'col5' returns 5
+    var currentColumn = board[currentColumnIndex]; // grabs the array that is selected 
     for (var i = 0; i < currentColumn.length; i++) { // currentColumn.length = 6
         // 0 1 2 3 4 5, loops 6 times
-        if (currentColumn[i] === '_') { // y, y, r, r
+        if (currentColumn[i] === '_') { // b, b, r, r
             currentColumn[i] = whosTurn;
             switchTurn();
             return render();
@@ -58,30 +55,34 @@ function handleClick(e) {
 
 // write a function to switch turns
 function switchTurn() {
-    if (whosTurn === player1) {
-        whosTurn = player2
+    if (whosTurn === player1) { //if whosTurn player 1 is true 
+        whosTurn = player2 // then set it to player 2
     } else {
-        whosTurn = player1
+        whosTurn = player1 // else its set back to player 1
     }
 }
 
 // if col0 was clicked then add disk to col0
 // function that renders state board values onto dom board
 function render() {
-    for (var i = 0; i < board.length; i++) {
+    for (var i = 0; i < board.length; i++) { // for loop that goes through board
         for (var j = 0; j < board[i].length; j++) {
-            // if slot is r log 'red', if slot is y log 'yellow'
+            // if slot is r log 'red', if slot is b log 'black'
             var slot = document.querySelectorAll(`#col${i} .row`)[j]
             if (board[i][j] === 'r') { // if the board coordinate is r then its red
-                slot.innerHTML = '<div class="disc red"></div>' //'#col0 .row' returns array of rows inside current columng
-            } else if (board[i][j] === 'y') { // if the board coordinate is not red hen its yellow
-                slot.innerHTML = '<div class="disc yellow"></div>' //'#col0 .row' returns array of rows inside current columng
+                slot.innerHTML = '<div class="disc red"></div>' //'#col0 .row' returns array of rows inside current column
+            } else if (board[i][j] === 'b') { // if the board coordinate is not red then its black
+                slot.innerHTML = '<div class="disc black"></div>' //'#col0 .row' returns array of rows inside current column
             } else { //if its not these things
-                slot.innerHTML = '' //'#col0 .row' returns array of rows inside current columng
+                slot.innerHTML = '' //'#col0 .row' returns array of rows inside current column
             }
         }
     }
 }
+
+/*----- win condition -----*/
+I need to check for rows, columns, and diagonaly. 
+using For loops, if statements and array indexing. 
 
 
 init();
