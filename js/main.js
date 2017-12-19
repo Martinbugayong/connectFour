8c0158ff7d0140c805cd2d94e20cac14ea2475cc
Martin Bugayong <martinbugayong@Martins-MacBook-Pro.local>
2017-12-19 10:42:33 -0800

    12/19/2017

--- js/main.js
+++ js/main.js
@@ -12,77 +12,101 @@ var board;
 var winner;
 
 /*----- cached element references -----*/
-
+var msgEl = document.getElementById('message');
 
 /*----- functions -----*/
 // Create an initialize function with a board array of columns and rows. 
 function init() {
     board = [
-        ['_', '_', '_', '_', '_', '_'],
-        ['_', '_', '_', '_', '_', '_'],
-        ['_', '_', '_', '_', '_', '_'],
-        ['_', '_', '_', '_', '_', '_'],
-        ['_', '_', '_', '_', '_', '_'],
-        ['_', '_', '_', '_', '_', '_'],
-        ['_', '_', '_', '_', '_', '_']
+        [null, null, null, null, null, null],
+        [null, null, null, null, null, null],
+        [null, null, null, null, null, null],
+        [null, null, null, null, null, null],
+        [null, null, null, null, null, null],
+        [null, null, null, null, null, null],
+        [null, null, null, null, null, null]
     ]
     whosTurn = player1;
     winner = false;
 }
 
 /*----- event listeners -----*/
-document.getElementById('col0').addEventListener('click', handleClick)
-document.getElementById('col1').addEventListener('click', handleClick)
-document.getElementById('col2').addEventListener('click', handleClick)
-document.getElementById('col3').addEventListener('click', handleClick)
-document.getElementById('col4').addEventListener('click', handleClick)
-document.getElementById('col5').addEventListener('click', handleClick)
-document.getElementById('col6').addEventListener('click', handleClick)
+document.getElementById('board').addEventListener('click', handleClick)
+// document.getElementById('col0').addEventListener('click', handleClick)
+// document.getElementById('col1').addEventListener('click', handleClick)
+// document.getElementById('col2').addEventListener('click', handleClick)
+// document.getElementById('col3').addEventListener('click', handleClick)
+// document.getElementById('col4').addEventListener('click', handleClick)
+// document.getElementById('col5').addEventListener('click', handleClick)
+// document.getElementById('col6').addEventListener('click', handleClick)
 
 function handleClick(e) {
-    var currentColumnId = e.currentTarget.id; // column id is set to current target clicked and will grab  the ID from the html
-    var currentColumnIndex = currentColumnId[3]; // which ever column is selected will return 3rd indexed character in string 'col5' returns 5
-    var currentColumn = board[currentColumnIndex]; // grabs the array that is selected 
-    for (var i = 0; i < currentColumn.length; i++) { // currentColumn.length = 6
-        // 0 1 2 3 4 5, loops 6 times
-        if (currentColumn[i] === '_') { // b, b, r, r
-            currentColumn[i] = whosTurn;
-            switchTurn();
-            return render();
-        }
-    }
+    var colDiv = e.target.id ? e.target : e.target.parentElement;
+    var colIdx = parseInt(colDiv.id.replace('col', ''));
+    var column = board[colIdx];
+    var firstAvalSlot = column.indexOf(null);
+    if (firstAvalSlot === -1) return;
+    column[firstAvalSlot] = whosTurn; 
+    whosTurn = (whosTurn === player1) ? player2 : player1;
+    /*----- check for winner -----*/
+    winner = getWinner();
+    render();
 }
 
 // write a function to switch turns
-function switchTurn() {
-    if (whosTurn === player1) { //if whosTurn player 1 is true 
-        whosTurn = player2 // then set it to player 2
-    } else {
-        whosTurn = player1 // else its set back to player 1
-    }
-}
+// function switchTurn() {
+//     if (whosTurn === player1) { //if whosTurn player 1 is true 
+//         whosTurn = player2 // then set it to player 2
+//     } else {
+//         whosTurn = player1 // else its set back to player 1
+//     }
+//     document.getElementById('message').innerHTML = "It's " + whosTurn + ' turn...'
+// }
 
 // if col0 was clicked then add disk to col0
 // function that renders state board values onto dom board
 function render() {
     for (var i = 0; i < board.length; i++) { // for loop that goes through board
+        var columnDivs = document.querySelectorAll(`#col${i} div`);
         for (var j = 0; j < board[i].length; j++) {
+            if (board[i][j]) columnDivs[j].style.backgroundColor = board[i][j] === 'r' ? 'red' : 'black';
             // if slot is r log 'red', if slot is b log 'black'
-            var slot = document.querySelectorAll(`#col${i} .row`)[j]
-            if (board[i][j] === 'r') { // if the board coordinate is r then its red
-                slot.innerHTML = '<div class="disc red"></div>' //'#col0 .row' returns array of rows inside current column
-            } else if (board[i][j] === 'b') { // if the board coordinate is not red then its black
-                slot.innerHTML = '<div class="disc black"></div>' //'#col0 .row' returns array of rows inside current column
-            } else { //if its not these things
-                slot.innerHTML = '' //'#col0 .row' returns array of rows inside current column
-            }
+            // if (board[i][j] === 'r') { // if the board coordinate is r then its red
+            //     slot.innerHTML = '<div class="disc red"></div>' //'#col0 .row' returns array of rows inside current column
+            // } else if (board[i][j] === 'b') { // if the board coordinate is not red then its black
+            //     slot.innerHTML = '<div class="disc black"></div>' //'#col0 .row' returns array of rows inside current column
+            // } else { //if its not these things
+            //     slot.innerHTML = '' //'#col0 .row' returns array of rows inside current column
+            // }
         }
+    }
+    if (winner) {
+        msgEl.innerHTML = 'Congrats, to player ' + (winner === player1 ? 'Red' : 'Black'); 
+    } else {
+        msgEl.innerHTML = 'Player\'s ' + (whosTurn === player1 ? 'Red' : 'Black') + ' Turn'; 
+
     }
 }
 
+function getWinner() {
+
+}
+
 /*----- win condition -----*/
-I need to check for rows, columns, and diagonaly. 
-using For loops, if statements and array indexing. 
+// I need to check for rows, columns, and diagonaly    
+// using For loops, if statements and array indexing.
+// var x = [
+//     ["a", "b", "c"], 
+//     ["d", "e", "f"], 
+//     ["g", "h", "i"]
+// ]
+
+// for (var row=2; row >=0; row--) {
+//     for (col=0; col < 3; col++) {
+//         console.log(`Coordinates are (${col}, ${row})`)
+//         console.log(x[row][col])
+//     }
+// }
 
 
 init();
